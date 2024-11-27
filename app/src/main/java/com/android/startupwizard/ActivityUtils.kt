ec471607c5a5378db17a328394b28ea0685675d4
dear_hj <dear_hj@163.com>
2024-11-27 14:30:06 +0800

    解决频繁切换页面导致应用崩溃问题

--- app/src/main/java/com/android/startupwizard/ActivityUtils.kt
+++ app/src/main/java/com/android/startupwizard/ActivityUtils.kt
@@ -1,23 +1,33 @@
 package com.android.startupwizard
 
 import android.app.Activity
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.launch
 
 object ActivityUtils {
+    private val scope = MainScope()
 
     private val activityStack: MutableList<Activity> = mutableListOf()
 
     fun addActivity(activity: Activity) {
-        activityStack.add(activity)
+        scope.launch(Dispatchers.Main) {
+            activityStack.add(activity)
+        }
     }
 
     fun removeActivity(activity: Activity) {
-        activityStack.remove(activity)
+        scope.launch(Dispatchers.Main) {
+            activityStack.remove(activity)
+        }
     }
 
     fun finishAllActivity() {
-        for (i in activityStack.indices) {
-            activityStack[i].finish()
+        scope.launch(Dispatchers.Main) {
+            for (i in activityStack.indices) {
+                activityStack[i].finish()
+            }
+            activityStack.clear()
         }
-        activityStack.clear()
     }
 }
